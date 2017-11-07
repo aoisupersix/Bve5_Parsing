87cfd512eaaf7df1cfcaa474acd2235c17dd6f7a
aoisupersix <r58460@st.takushoku-u.ac.jp>
2017-11-07 22:06:36 +0900

    [add] CurveNodesの各AST木を追加

--- IronyTest/MapGrammars/AstNodes/CurveNodes/CurveNodes.cs
+++ IronyTest/MapGrammars/AstNodes/CurveNodes/CurveNodes.cs
@@ -27,6 +27,51 @@ namespace IronyTest.MapGrammars.AstNodes.CurveNodes
     }
 
     /// <summary>
+    /// Curve.SetCenter(x)
+    /// </summary>
+    public class SetCenterNode : Syntax_1
+    {
+        public override void Init(AstContext context, ParseTreeNode treeNode)
+        {
+            base.Init(context, treeNode);
+            ParseTreeNodeList nodes = treeNode.GetMappedChildNodes();
+
+            //引数の登録
+            AddArguments("x", nodes[2]);
+        }
+    }
+
+    /// <summary>
+    /// Curve.SetFunction(id)
+    /// </summary>
+    public class SetFunctionNode : Syntax_1
+    {
+        public override void Init(AstContext context, ParseTreeNode treeNode)
+        {
+            base.Init(context, treeNode);
+            ParseTreeNodeList nodes = treeNode.GetMappedChildNodes();
+
+            //引数の登録
+            //idは0か1のみなのでその判定 TODO
+            AddArguments("id", nodes[2]);
+        }
+    }
+
+    /// <summary>
+    /// Curve.BeginTransition()
+    /// </summary>
+    public class BeginTransitionNode : Syntax_1
+    {
+        public override void Init(AstContext context, ParseTreeNode treeNode)
+        {
+            base.Init(context, treeNode);
+            ParseTreeNodeList nodes = treeNode.GetMappedChildNodes();
+
+            //引数なし
+        }
+    }
+
+    /// <summary>
     /// Curve.Begin(radius, cant) | Curve.Begin(radius)
     /// </summary>
     public class BeginNode : Syntax_1
@@ -40,6 +85,59 @@ namespace IronyTest.MapGrammars.AstNodes.CurveNodes
             AddArguments("radius", nodes[2]);
             if (nodes.Count > 3)
                 AddArguments("cant", nodes[3]);
+        }
+    }
+
+    /// <summary>
+    /// Curve.End()
+    /// </summary>
+    public class EndNode : Syntax_1
+    {
+        public override void Init(AstContext context, ParseTreeNode treeNode)
+        {
+            base.Init(context, treeNode);
+            ParseTreeNodeList nodes = treeNode.GetMappedChildNodes();
+
+            //引数なし
+        }
+    }
+
+    /// <summary>
+    /// Curve.Interpolate(radius, cant) | Curve.Interpolate(radius) | Curve.Interpolate()
+    /// </summary>
+    public class InterpolateNode : Syntax_1
+    {
+        public override void Init(AstContext context, ParseTreeNode treeNode)
+        {
+            base.Init(context, treeNode);
+            ParseTreeNodeList nodes = treeNode.GetMappedChildNodes();
+
+            //引数の登録
+            if(nodes.Count > 2)
+            {
+                //引数:radius
+                AddArguments("radius", nodes[2]);
+
+                if (nodes.Count > 3)
+                    //引数:cant
+                    AddArguments("cant", nodes[3]);
+            }
+            //引数なし
+        }
+    }
+
+    /// <summary>
+    /// Curve.Change(radius)
+    /// </summary>
+    public class ChangeNode : Syntax_1
+    {
+        public override void Init(AstContext context, ParseTreeNode treeNode)
+        {
+            base.Init(context, treeNode);
+            ParseTreeNodeList nodes = treeNode.GetMappedChildNodes();
+
+            //引数の登録
+            AddArguments("radius", nodes[2]);
         }
     }
 }
