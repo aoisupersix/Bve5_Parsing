bb2f47463789bac2cf30a4b5e5e16e10452f54ab
aoisupersix <r58460@st.takushoku-u.ac.jp>
2017-11-08 10:29:11 +0900

    [add] PreTrain構文のAST木を追加

---
+++ IronyTest/MapGrammars/AstNodes/PreTrainNodes/PreTrainNodes.cs
@@ -0,0 +1,35 @@
+﻿using Irony.Ast;
+using Irony.Parsing;
+
+namespace IronyTest.MapGrammars.AstNodes.PreTrainNodes
+{
+    /*
+     * PreTrainのAST木定義
+     */
+
+    /// <summary>
+    /// PreTrain.Pass(hh:mm:ss) | PreTrain.Pass(second)
+    /// </summary>
+    public class BeginNode : Syntax_1
+    {
+        public override void Init(AstContext context, ParseTreeNode treeNode)
+        {
+            base.Init(context, treeNode);
+            ParseTreeNodeList nodes = treeNode.GetMappedChildNodes();
+
+            //引数の登録
+            if(nodes.Count > 3)
+            {
+                //hh:mm:ss TODO
+                AddArguments("hh", nodes[2]);
+                AddArguments("mm", nodes[2]);
+                AddArguments("ss", nodes[2]);
+            }
+            else
+            {
+                //sec
+                AddArguments("second", nodes[2]);
+            }
+        }
+    }
+}
