1826f9a9b8bcf3409539a7e883e652cf385200d8
aoisupersix <r58460@st.takushoku-u.ac.jp>
2017-11-11 21:49:49 +0900

    [update] ScenarioGrammarの細かい修正

--- Bve5_Parsing/ScenarioGrammar/ScenarioGrammar.cs
+++ Bve5_Parsing/ScenarioGrammar/ScenarioGrammar.cs
@@ -7,60 +7,60 @@ using Irony.Interpreter.Ast;
 
 namespace IronyTest
 {
-    [Language("ScenariosGrammar")]
-    class ScenariosGrammar : InterpretedLanguageGrammar
+    [Language("ScenarioGrammar")]
+    class ScenarioGrammar : InterpretedLanguageGrammar
     {
-        public ScenariosGrammar() : base(true)
+        public ScenarioGrammar() : base(false)
         {
-            ////終端記号の定義
-            //var filePath = new IdentifierTerminal("filePath", ExtraChars.MULTIBYTES + ExtraChars.TOKEN + @"\", ExtraChars.MULTIBYTES + ExtraChars.TOKEN);
-            //var text = new IdentifierTerminal("text", ExtraChars.MULTIBYTES + ExtraChars.TOKEN + @"\s" + "\"", ExtraChars.MULTIBYTES + ExtraChars.TOKEN);
-            //var equal = ToTerm("=");
+            //終端記号の定義
+            var filePath = new IdentifierTerminal("filePath", ExtraChars.MULTIBYTES + ExtraChars.TOKEN + @"\", ExtraChars.MULTIBYTES + ExtraChars.TOKEN);
+            var text = new IdentifierTerminal("text", ExtraChars.MULTIBYTES + ExtraChars.TOKEN + @"\s" + "\"", ExtraChars.MULTIBYTES + ExtraChars.TOKEN);
+            var equal = ToTerm("=");
 
 
-            ////非終端記号の定義
-            //var end = new NonTerminal("End", typeof(EndNode));
-            //end.Rule = NewLine | Eof;
+            //非終端記号の定義
+            var end = new NonTerminal("End", typeof(EndNode));
+            end.Rule = NewLine | Eof;
 
-            //var route = new NonTerminal("Route", typeof(PathNode));
-            //route.Rule = "Route" + equal + filePath + end | "Route" + equal + end;
+            var route = new NonTerminal("Route", typeof(PathNode));
+            route.Rule = "Route" + equal + filePath + end | "Route" + equal + end;
 
-            //var vehicle = new NonTerminal("Vehicle", typeof(PathNode));
-            //vehicle.Rule = "Vehicle" + equal + filePath + end | "Vehicle" + equal + end;
+            var vehicle = new NonTerminal("Vehicle", typeof(PathNode));
+            vehicle.Rule = "Vehicle" + equal + filePath + end | "Vehicle" + equal + end;
 
-            //var image = new NonTerminal("Image", typeof(PathNode));
-            //image.Rule = "Image" + equal + filePath + end | "Image" + equal + end;
+            var image = new NonTerminal("Image", typeof(PathNode));
+            image.Rule = "Image" + equal + filePath + end | "Image" + equal + end;
 
-            //var title = new NonTerminal("Title", typeof(TextNode));
-            //title.Rule = "Title" + equal + text + end | "Title" + equal + end;
+            var title = new NonTerminal("Title", typeof(TextNode));
+            title.Rule = "Title" + equal + text + end | "Title" + equal + end;
 
-            //var routeTitle = new NonTerminal("RouteTitle", typeof(TextNode));
-            //routeTitle.Rule = "RouteTitle" + equal + text + end | "RouteTitle" + equal + end;
+            var routeTitle = new NonTerminal("RouteTitle", typeof(TextNode));
+            routeTitle.Rule = "RouteTitle" + equal + text + end | "RouteTitle" + equal + end;
 
-            //var vehicleTitle = new NonTerminal("VehicleTitle", typeof(TextNode));
-            //vehicleTitle.Rule = "VehicleTitle" + equal + text + end | "VehicleTitle" + equal + end ;
+            var vehicleTitle = new NonTerminal("VehicleTitle", typeof(TextNode));
+            vehicleTitle.Rule = "VehicleTitle" + equal + text + end | "VehicleTitle" + equal + end;
 
-            //var author = new NonTerminal("Author", typeof(TextNode));
-            //author.Rule = "Author" + equal + text + end | "Author" + equal + end;
+            var author = new NonTerminal("Author", typeof(TextNode));
+            author.Rule = "Author" + equal + text + end | "Author" + equal + end;
 
-            //var scenarioComment = new NonTerminal("ScenarioComment", typeof(TextNode));
-            //scenarioComment.Rule = "Comment" + equal + text + end | "Comment" + equal + end;
+            var scenarioComment = new NonTerminal("ScenarioComment", typeof(TextNode));
+            scenarioComment.Rule = "Comment" + equal + text + end | "Comment" + equal + end;
 
-            //var statement = new NonTerminal("Statement", typeof(StatementNode));
-            //statement.Rule = route | vehicle | image | title | routeTitle | vehicleTitle | author | scenarioComment | NewLine;
+            var statement = new NonTerminal("Statement", typeof(StatementNode));
+            statement.Rule = route | vehicle | image | title | routeTitle | vehicleTitle | author | scenarioComment | NewLine;
 
-            //var program = new NonTerminal("Program", typeof(ProgramNode));
-            //program.Rule = MakeStarRule(program, statement);
+            var program = new NonTerminal("Program", typeof(ProgramNode));
+            program.Rule = MakeStarRule(program, statement);
 
-            //Root = program;
+            Root = program;
 
-            ////コメント
-            //var comment1 = new CommentTerminal("Comment#", "#", "\n", "\r");
-            //var comment2 = new CommentTerminal("Comment;", ";", "\n", "\r");
-            //this.NonGrammarTerminals.Add(comment1);
-            //this.NonGrammarTerminals.Add(comment2);
+            //コメント
+            var comment1 = new CommentTerminal("Comment#", "#", "\n", "\r");
+            var comment2 = new CommentTerminal("Comment;", ";", "\n", "\r");
+            this.NonGrammarTerminals.Add(comment1);
+            this.NonGrammarTerminals.Add(comment2);
 
-            //LanguageFlags = LanguageFlags.CreateAst;
+            LanguageFlags = LanguageFlags.CreateAst;
         }
     }
     public class EndNode : AstNode
