6a0c248b1810dd895d754829db1b94486d514497
aoisupersix <r58460@st.takushoku-u.ac.jp>
2017-11-12 16:18:47 +0900

    [change] 文法のテストのためコンソールアプリに変更

--- Bve5_Parsing/GrammarTest.cs
+++ Bve5_Parsing/GrammarTest.cs
@@ -7,26 +7,26 @@ namespace IronyTest
 {
     class GrammarTest
     {
-        //static void Main(String[] args)
-        //{
-        //    Console.WriteLine("ScenariosGrammarTest");
-        //    string scenario = "";
+        static void Main(String[] args)
+        {
+            Console.WriteLine("ScenariosGrammarTest");
+            string scenario = "";
 
-        //    try
-        //    {
-        //        ScriptApp app = new ScriptApp(new LanguageData(new ScenariosGrammar()));
-        //        Dictionary<string, string> result = (Dictionary<string, string>)app.Evaluate(scenario);
-        //        foreach(var p in result)
-        //        {
-        //            Console.WriteLine("Key: {0} -> Val: {1}", p.Key, p.Value);
-        //        }
-        //    }
-        //    catch(ScriptException e)
-        //    {
-        //        Console.Error.WriteLine("{0} {1}", e.Location, e.Message);
-        //    }
+            try
+            {
+                ScriptApp app = new ScriptApp(new LanguageData(new ScenariosGrammar()));
+                Dictionary<string, string> result = (Dictionary<string, string>)app.Evaluate(scenario);
+                foreach (var p in result)
+                {
+                    Console.WriteLine("Key: {0} -> Val: {1}", p.Key, p.Value);
+                }
+            }
+            catch (ScriptException e)
+            {
+                Console.Error.WriteLine("{0} {1}", e.Location, e.Message);
+            }
 
-        //    Console.ReadLine();
-        //}
+            Console.ReadLine();
+        }
     }
 }
