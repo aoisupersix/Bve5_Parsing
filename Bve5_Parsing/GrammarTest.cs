e4ad3ae82a635c477a3076a2f28285426ea001a2
aoisupersix <r58460@st.takushoku-u.ac.jp>
2017-11-14 11:00:08 +0900

    [upate] GrammarTestの実装

--- Bve5_Parsing/GrammarTest.cs
+++ Bve5_Parsing/GrammarTest.cs
@@ -9,23 +9,52 @@ namespace Bve5_Parsing
     {
         static void Main(String[] args)
         {
-            Console.WriteLine("ScenariosGrammarTest");
-            string scenario = "BveTs Scenario 1.00";
-
-            try
+            bool end = false;
+            while (!end)
             {
-                ScriptApp app = new ScriptApp(new LanguageData(new ScenarioGrammar.ScenarioGrammar()));
-                Dictionary<string, string> result = (Dictionary<string, string>)app.Evaluate(scenario);
-                foreach (var p in result)
+                Console.WriteLine("MapGrammar_Careless Test");
+                Console.WriteLine("====================================");
+
+                string input = "";
+                Console.WriteLine("Input your syntax. Enter \"EOF\" to end input.");
+                string line = Console.ReadLine();
+                while (!line.Equals("EOF"))
                 {
-                    Console.WriteLine("Key: {0} -> Val: {1}", p.Key, p.Value);
+                    input += line + Environment.NewLine;
+                    line = Console.ReadLine();
+                }
+
+                Console.WriteLine("====================================");
+                Console.WriteLine("Parser Output:");
+
+                try
+                {
+                    ScriptApp app = new ScriptApp(new LanguageData(new MapGrammar_Careless.MapGrammar_Careless()));
+                    MapGrammar_Careless.MapData result = (MapGrammar_Careless.MapData)app.Evaluate(input);
+
+                    //結果表示
+                    Console.WriteLine("Version:{0}", result.Version);
+                    Console.WriteLine("Encoding:{0}", result.Encoding);
+                    Console.WriteLine("StructureListPath:{0}", result.StructureListPath);
+                    Console.WriteLine("StationListPath:{0}", result.StationListPath);
+                    Console.WriteLine("SignalListPath:{0}", result.SignalListPath);
+                    Console.WriteLine("SoundListPath:{0}", result.SoundListPath);
+                    Console.WriteLine("Sound3DListPath:{0}", result.Sound3DListPath);
+
+                }
+                catch (ScriptException e)
+                {
+
+                    Console.Error.WriteLine("{0}: {1}", e.Location, e.Message);
+                }
+
+                Console.WriteLine("End it? Y/N");
+                string endInput = Console.ReadLine();
+                if (endInput.Equals("Y"))
+                {
+                    end = true;
                 }
             }
-            catch (ScriptException e)
-            {
-                Console.Error.WriteLine("{0} {1}", e.Location, e.Message);
-            }
-
             Console.ReadLine();
         }
     }
