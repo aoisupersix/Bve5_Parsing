0d2bb3a395e7229b7d469fd7048d2c7194845fc9
aoisupersix <r58460@st.takushoku-u.ac.jp>
2017-12-19 22:31:58 +0900

    [add] ScenarioGrammarAstNodeを追加

---
+++ Bve5_Parsing/ScenarioGrammar/AstNodes/AstNodes.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bve5_Parsing.ScenarioGrammar.AstNodes
+{
+    /**
+     * ScenarioGrammarのAstNode定義
+     */
+
+    /// <summary>
+    /// AST親クラス
+    /// </summary>
+    internal abstract class ScenarioGrammarAstNodes { }
+
+    /// <summary>
+    /// ルートノード
+    /// </summary>
+    internal class RootNode : ScenarioGrammarAstNodes
+    {
+        public List<ScenarioGrammarAstNodes> StatementList { get; set; }
+        public string Version { get; set; }
+        public string Encoding { get; set; }
+        public RootNode()
+        {
+            StatementList = new List<ScenarioGrammarAstNodes>();
+        }
+    }
+
+    /// <summary>
+    /// ステートメント親ノード
+    /// </summary>
+    internal abstract class StatementNode : ScenarioGrammarAstNodes
+    {
+        public string StateName { get; set; }
+    }
+}
