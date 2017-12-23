f37c3fd38ab1841ad9cdebf30e8b780dbb278c32
aoisupersix <r58460@st.takushoku-u.ac.jp>
2017-12-23 14:55:08 +0900

    [add] ScenarioGrammarのパーサクラスを追加

---
+++ Bve5_Parsing/ScenarioGrammar/ScenarioGrammarParser.cs
@@ -0,0 +1,26 @@
+﻿using Antlr4.Runtime;
+using Bve5_Parsing.ScenarioGrammar.AstNodes;
+using Bve5_Parsing.ScenarioGrammar.SyntaxDefinitions;
+
+namespace Bve5_Parsing.ScenarioGrammar
+{
+    /// <summary>
+    /// ScenarioGrammarの解析を行うクラス
+    /// </summary>
+    class ScenarioGrammarParser
+    {
+        public ScenarioData Parse(string input)
+        {
+            AntlrInputStream inputStream = new AntlrInputStream(input);
+            ScenarioGrammarLexer lexer = new ScenarioGrammarLexer(inputStream);
+            CommonTokenStream commonTokenStream = new CommonTokenStream(lexer);
+            SyntaxDefinitions.ScenarioGrammarParser parser = new SyntaxDefinitions.ScenarioGrammarParser(commonTokenStream);
+
+            ScenarioData value = null;
+            var cst = parser.root();
+            var ast = new BuildAstVisitor().VisitRoot(cst);
+
+            return value;
+        }
+    }
+}
