ed1c525132515e5a1bb6ecab3a10eeb7d72ddbfb
aoisupersix <aoi_supersix@yahoo.co.jp>
2017-12-28 12:14:12 +0900

    [add] ScenarioGrammarのAST評価クラスを追加

--- Bve5_Parsing/ScenarioGrammar/AstEvaluator.cs
+++ Bve5_Parsing/ScenarioGrammar/AstEvaluator.cs
@@ -24,4 +24,103 @@ namespace Bve5_Parsing.ScenarioGrammar
             return Visit((dynamic)node);
         }
     }
+
+    internal class EvaluateScenarioGrammarVisitor : AstVisitor<object>
+    {
+        /// <summary>
+        /// 評価結果
+        /// </summary>
+        private ScenarioData evaluateData;
+
+        /// <summary>
+        /// ルートノードの評価
+        /// </summary>
+        /// <param name="node">ルートノード</param>
+        /// <returns>解析結果のScenarioData</returns>
+        public override object Visit(RootNode node)
+        {
+            ScenarioData evaluateData = new ScenarioData
+            {
+                Version = node.Version
+            };
+
+            //ステートメントの巡回(出力は各ステートメントごとに行う)
+            foreach(var statement in node.StatementList)
+            {
+                if(statement != null)
+                    Visit(statement);
+            }
+
+            return evaluateData;
+        }
+
+        /// <summary>
+        /// 重み付けステートメントノードの評価
+        /// </summary>
+        /// <param name="node">重み付けステートメントノード</param>
+        /// <returns>戻り値なし</returns>
+        public override object Visit(WeightStateNode node)
+        {
+            switch (node.StateName)
+            {
+                case "route":
+                    foreach(var path in node.PathList)
+                        evaluateData.Route.Add((FilePath)Visit(path));
+                    break;
+                case "vehicle":
+                    foreach (var path in node.PathList)
+                        evaluateData.Vehicle.Add((FilePath)Visit(path));
+                    break;
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// テキストステートメントノードの評価
+        /// </summary>
+        /// <param name="node">テキストステートメントノード</param>
+        /// <returns>戻り値なし</returns>
+        public override object Visit(TextStateNode node)
+        {
+            switch (node.StateName)
+            {
+                case "image":
+                    evaluateData.Image = node.Text;
+                    break;
+                case "title":
+                    evaluateData.Title = node.Text;
+                    break;
+                case "routetitle":
+                    evaluateData.RouteTitle = node.Text;
+                    break;
+                case "vehicletitle":
+                    evaluateData.VehicleTitle = node.Text;
+                    break;
+                case "author":
+                    evaluateData.Author = node.Text;
+                    break;
+                case "comment":
+                    evaluateData.Comment = node.Text;
+                    break;
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// 重み付けファイルパスノードの評価
+        /// </summary>
+        /// <param name="node">重み付けファイルパスノード</param>
+        /// <returns>ファイルパス構造体</returns>
+        public override object Visit(WeightPathNode node)
+        {
+            FilePath filePath = new FilePath
+            {
+                Value = node.Path,
+                Weight = node.Weight
+            };
+            return filePath;
+        }
+    }
 }
