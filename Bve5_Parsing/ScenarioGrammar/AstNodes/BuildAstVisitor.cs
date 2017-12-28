a12bd6df729ad9075fce8a42bbd372fcde16d8da
aoisupersix <aoi_supersix@yahoo.co.jp>
2017-12-28 11:14:02 +0900

    [add] ImageStatementの巡回を実装

--- Bve5_Parsing/ScenarioGrammar/AstNodes/BuildAstVisitor.cs
+++ Bve5_Parsing/ScenarioGrammar/AstNodes/BuildAstVisitor.cs
@@ -83,6 +83,16 @@ namespace Bve5_Parsing.ScenarioGrammar.AstNodes
             return new TextStateNode { StateName = context.stateName.Text.ToLower(), Text = context.@string().GetText() };
         }
 
+        /// <summary>
+        /// サムネイルステートメントの巡回
+        /// </summary>
+        /// <param name="context">構文解析の文脈データ</param>
+        /// <returns>テキストASTノード</returns>
+        public override ScenarioGrammarAstNodes VisitImageState([NotNull] SyntaxDefinitions.ScenarioGrammarParser.ImageStateContext context)
+        {
+            return new TextStateNode { StateName = context.stateName.Text.ToLower(), Text = context.@string().GetText() };
+        }
+
         #endregion ステートメントの巡回
 
         /// <summary>
