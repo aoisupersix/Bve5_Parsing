ac8cf76998ee1a4935b949c57e5c1388d112ee16
aoisupersix <aoisupersix@gmail.com>
2018-10-20 18:21:12 +0900

    [add] テスト追加

--- Bve5_Parsing/MapGrammar/MapData.cs
+++ Bve5_Parsing/MapGrammar/MapData.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Bve5_Parsing.MapGrammar
 {
@@ -7,6 +8,7 @@ namespace Bve5_Parsing.MapGrammar
     /// </summary>
     public class MapData
     {
+        #region プロパティ
         /// <summary>
         /// マップファイルのバージョン
         /// </summary>
@@ -46,11 +48,55 @@ namespace Bve5_Parsing.MapGrammar
         /// 構文
         /// </summary>
         public List<SyntaxData> Statements { get; set; }
+        #endregion
 
+        /// <summary>
+        /// インスタンスを生成します。
+        /// </summary>
         public MapData()
         {
             Statements = new List<SyntaxData>();
         }
+
+        #region Override
+        /// <summary>
+        /// 等価チェック
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public override bool Equals(object obj)
+        {
+            if (obj == null || this.GetType() != obj.GetType())
+                return false;
+
+            var m = (MapData)obj;
+
+            return
+                m.Version == Version &&
+                m.Encoding == Encoding &&
+                m.StructureListPath == StructureListPath &&
+                m.StationListPath == StationListPath &&
+                m.SignalListPath == SignalListPath &&
+                m.SoundListPath == SoundListPath &&
+                m.Sound3DListPath == m.Sound3DListPath &&
+                m.Statements.SequenceEqual(Statements)
+                ;
+        }
+
+        public override int GetHashCode()
+        {
+            return
+                Version.GetHashCode() ^
+                Encoding.GetHashCode() ^
+                StructureListPath.GetHashCode() ^
+                StationListPath.GetHashCode() ^
+                SignalListPath.GetHashCode() ^
+                SoundListPath.GetHashCode() ^
+                Sound3DListPath.GetHashCode() ^
+                Statements.GetHashCode()
+                ;
+        }
+        #endregion
 
         /// <summary>
         /// 文字列から対応するリストファイルのパスを設定する
@@ -96,5 +142,38 @@ namespace Bve5_Parsing.MapGrammar
             Arguments = new Dictionary<string, object>();
         }
 
+        #region Override
+        /// <summary>
+        /// 等価チェック
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public override bool Equals(object obj)
+        {
+            if (obj == null || this.GetType() != obj.GetType())
+                return false;
+
+            var s = (SyntaxData)obj;
+
+            return
+                s.Distance == Distance &&
+                ((s.MapElement == null && MapElement == null ) || (s.MapElement != null && MapElement != null && s.MapElement.SequenceEqual(MapElement))) &&
+                s.Key == Key &&
+                s.Function == Function &&
+                s.Arguments.SequenceEqual(Arguments) //Argumentsは必ずnullではない
+                ;
+        }
+
+        public override int GetHashCode()
+        {
+            return
+                Distance.GetHashCode() ^
+                MapElement.GetHashCode() ^
+                Key.GetHashCode() ^
+                Function.GetHashCode() ^
+                Arguments.GetHashCode()
+                ;
+        }
+        #endregion
     }
 }
