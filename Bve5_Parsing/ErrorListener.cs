db05f757829d3b12c623767483a532dad546ee80
aoisupersix <aoisupersix@gmail.com>
2018-10-23 13:10:52 +0900

    [update] Consoleに出力するエラーメッセージを修正

--- Bve5_Parsing/ErrorListener.cs
+++ Bve5_Parsing/ErrorListener.cs
@@ -10,9 +10,39 @@ namespace Bve5_Parsing
     /// </summary>
     public class ParseErrorListener : BaseErrorListener
     {
+        #region フィールド
+        /// <summary>
+        /// InputMismatchExceptionのエラーメッセージ
+        /// </summary>
+        public const string ERRMSG_INPUT_MISMATCH = "行{0},列{1}: 入力文字列\"{2}\"がマップ構文と一致しませんでした。";
+
+        /// <summary>
+        /// NoViableExceptionのエラーメッセージ
+        /// </summary>
+        public const string ERRMSG_NO_VIABLE = "行{0},列{1}: 入力文字列\"{2}\"の構文を特定できませんでした。";
+        #endregion
+
+        #region エラーメッセージ生成
+        private string GetErrorMessage(IRecognizer recognizer, IToken token, int line, int charPositionInLine, InputMismatchException e)
+        {
+            return string.Format(ERRMSG_INPUT_MISMATCH, line, charPositionInLine, token.Text);
+        }
+
+        private string GetErrorMessage(IRecognizer recognizer, IToken token, int line, int charPositionInLine, NoViableAltException e)
+        {
+            return string.Format(ERRMSG_NO_VIABLE, line, charPositionInLine, token.Text);
+        }
+
+        private string GetErrorMessage(IRecognizer recognizer, IToken token, int line, int charPositionInLine, LexerNoViableAltException e)
+        {
+            return string.Format(ERRMSG_NO_VIABLE, line, charPositionInLine, token.Text);
+        }
+        #endregion
+
         public override void SyntaxError(IRecognizer recognizer, IToken offendingSymbol, int line, int charPositionInLine, string msg, RecognitionException e)
         {
-            Console.Error.WriteLine(msg);
+            var m = GetErrorMessage(recognizer, offendingSymbol, line, charPositionInLine, (dynamic)e.GetBaseException());
+            Console.Error.WriteLine(m);
         }
     }
 }
