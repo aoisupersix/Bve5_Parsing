412ae0c73b80dfb49d107fee8c764d8f372cd9a4
aoisupersix <aoisupersix@gmail.com>
2019-04-07 10:19:03 +0900

    [add] Jsonにマップ要素名と関数名を取得する処理を追加

--- doc/createMapGrammarTemplate.js
+++ doc/createMapGrammarTemplate.js
@@ -1,57 +1,75 @@
-var fs = require('fs');
-var stripBom = require('strip-bom');
-var mustache = require('mustache');
+const fs = require('fs');
+const linq = require('linq');
+const stripBom = require('strip-bom');
+const mustache = require('mustache');
 
-parse("doc/map_grammar_ast.mst", "doc/map_grammar_syntax.json", "Bve5_Parsing/MapGrammar/AstNodes/SyntaxNodes.cs");
+const jsonData = loadJson("doc/map_grammar_syntax.json");
 
-function parse(template, json, output) {
-  fs.readFile(template, 'utf-8', function (err, template) {
+// Ast生成
+const astTemp = loadFile("doc/map_grammar_ast.mst");
+parse(astTemp, jsonData, "Bve5_Parsing/MapGrammar/AstNodes/SyntaxNodes.cs");
+
+console.log("all completed !");
+
+function loadJson(jsonPath) {
+  const json = JSON.parse(loadFile(jsonPath));
+  let elems = [];
+  let funcs = [];
+  //Jsonの前処理
+  json.states.forEach(state => {
+    // 構文種別の判定
+    if (state.key === undefined) {
+      state["syntax1"] = true;
+      state["syntax2"] = false;
+      state["syntax3"] = false;
+    }else if(state.elem2 === undefined) {
+      state["syntax1"] = false;
+      state["syntax2"] = true;
+      state["syntax3"] = false;
+    }else {
+      state["syntax1"] = false;
+      state["syntax2"] = false;
+      state["syntax3"] = true;
+    }
+
+    // 引数があるか
+    if (state.args.length == 0) {
+      state["noarg"] = true;
+    }else {
+      state.args.slice(-1)[0]["last"] = true;
+    }
+
+    // マップ要素名取得
+    if (linq.from(elems).all(e => e != state.elem)) {
+      elems.push(state.elem);
+    }
+
+    // 関数名取得
+    let funcName = state.func;
+    if (state.syntax3 == true) {
+      funcName = state.elem2 + "_" + funcName;
+    }
+    if (linq.from(funcs).all(f => f != funcName)) {
+      funcs.push(funcName);
+    }
+  });
+
+  json["elems"] = elems;
+  json["funcs"] = funcs;
+  return json
+}
+
+function loadFile(filePath) {
+  return fs.readFileSync(filePath, 'utf-8');
+}
+
+function parse(template, jsonData, outputPath) {
+  const output = mustache.render(stripBom(template), jsonData);
+  fs.writeFile(outputPath, output, 'utf-8', (err) => {
     if (err) {
       console.error(err);
       process.exit(1);
     }
-    else {
-      fs.readFile(json, 'utf-8', function (err, data) {
-        if (err) {
-          console.error(err);
-          process.exit(1);
-        }
-        else {
-          // Jsonの前処理
-          var jsonData = JSON.parse(stripBom(data));
-          jsonData.states.forEach(state => {
-            // 構文種別の判定
-            if (state.key === undefined) {
-              state["syntax1"] = true;
-              state["syntax2"] = false;
-              state["syntax3"] = false;
-            }else if(state.elem2 === undefined) {
-              state["syntax1"] = false;
-              state["syntax2"] = true;
-              state["syntax3"] = false;
-            }else {
-              state["syntax1"] = false;
-              state["syntax2"] = false;
-              state["syntax3"] = true;
-            }
-            if (state.args.length == 0) {
-              state["noarg"] = true;
-            }else {
-              state.args.slice(-1)[0]["last"] = true;
-            }
-          })
-          var result = mustache.render(stripBom(template), jsonData);
-          fs.writeFile(output, result, 'utf-8', function (err) {
-            if (err) {
-              console.error(err);
-              process.exit(1);
-            }
-            else {
-              console.log('finished!!');
-            }
-          });
-        }
-      });
-    }
+    console.log(outputPath + " generation completed.");
   });
 }
