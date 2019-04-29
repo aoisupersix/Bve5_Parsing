525bf37f31963aecc907803ddb550b0a7f89b92f
aoisupersix <aoisupersix@gmail.com>
2019-04-29 22:44:45 +0900

    [add] MapGrammarParserTestをファイルだけ追加
    テストはまだ未追加

---
+++ Bve5_ParsingTests/MapGrammarParserTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Bve5_ParsingTests
+{
+    public class MapGrammarParserTests
+    {
+        [Fact]
+        public void ParserOptionTest()
+        {
+
+        }
+    }
+}
