3f704020aef2c958b712e00026a767819a3091e8
aoisupersix <aoisupersix@gmail.com>
2019-05-16 13:35:09 +0900

    [update] マップ構文の定義をyamlに変更

--- gen/src/create.ts
+++ gen/src/create.ts
@@ -1,12 +1,11 @@
 import * as fs from 'fs'
+import * as yaml from 'js-yaml'
 
 function hello(name: string): string {
     return `Hello, ${name}!`
 }
 
-const file = fs.readFileSync(`${__dirname}/mapgrammar.json`, "utf8")
-const json = JSON.parse(file)
+const file = fs.readFileSync(`${__dirname}/mapgrammar.yaml`, "utf8")
+const mapDef = yaml.safeLoad(file)
 
-console.log(json)
-
-console.log(hello("TypeScript"))
+console.log(mapDef)
