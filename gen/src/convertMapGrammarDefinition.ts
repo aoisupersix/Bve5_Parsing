4ecf1da5915a3ef21050eb3467fc0292566dc74f
aoisupersix <aoisupersix@gmail.com>
2019-05-17 23:19:55 +0900

    [add] テスト値生成の外側だけ作成

--- gen/src/convertMapGrammarDefinition.ts
+++ gen/src/convertMapGrammarDefinition.ts
@@ -1,11 +1,62 @@
 import { MapStatement } from './mapGrammarDefinition'
 
 /**
- * マップ構文定義ファイルから色々な情報を付加した中間出力を生成します。
+ * 引数の型
  */
-export function convert(mapGrammarData: MapStatement[]) : void {
-  mapGrammarData.map(setSyntaxType)
+export enum ArgType {
+  /**
+   * 文字列
+   */
+  String = 'string',
+
+  /**
+   * 浮動小数点数
+   */
+  Double = 'double',
 }
 
-function setSyntaxType(element: object, index: number, array: object[]) {
+/**
+ * マップ構文定義ファイルから色々な情報を付加した中間出力を生成します。
+ */
+export function convert(mapGrammarData: MapStatement[]) : MapStatement[] {
+  const result = mapGrammarData.map(setSyntaxType)
+
+  return result
+}
+
+/**
+ * 構文種別をステートメントに設定します。
+ */
+function setSyntaxType(element: MapStatement, index: number, array: MapStatement[]): MapStatement {
+  if (element.key === undefined) {
+    element.syntax1 = true
+    element.syntax2 = false
+    element.syntax3 = false
+  }else if (element.sub_elem === undefined) {
+    element.syntax1 = false
+    element.syntax2 = true
+    element.syntax3 = false
+  }else {
+    element.syntax1 = false
+    element.syntax2 = false
+    element.syntax3 = true
+  }
+  return element
+}
+
+/**
+ * テスト値を生成して引数に設定します。
+ */
+function setTestValue(element: MapStatement, index: number, array: MapStatement[]): MapStatement {
+  element.args.forEach((val, idx, ary) => {
+    const nullable = val.type.slice(-1) === '?' // null許容かどうか. 今は特に処理なし
+    switch (val.type.replace('?', '').toLowerCase()) {
+      case ArgType.String:
+        break
+      case ArgType.Double:
+        break
+    }
+  })
+
+  return element
 }
--- gen/src/create.ts
+++ gen/src/create.ts
@@ -1,12 +1,13 @@
 import * as fs from 'fs'
 import * as yaml from 'js-yaml'
 import { MapStatement } from './mapGrammarDefinition';
-
-function hello(name: string): string {
-  return `Hello, ${name}!`
-}
+import { convert } from './convertMapGrammarDefinition';
 
 const file = fs.readFileSync(`${__dirname}/mapgrammar.yaml`, 'utf8')
 const mapDef: MapStatement[] = yaml.safeLoad(file)
+const convertedMapDef = convert(mapDef)
+const json = JSON.stringify(convertedMapDef)
 
-console.log(mapDef)
+console.log(json)
+
+console.log(convertedMapDef)
