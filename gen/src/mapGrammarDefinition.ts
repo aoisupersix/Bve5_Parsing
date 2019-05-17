f6aa2b31fa84e4ed2e93eba76aec982fbf66874b
aoisupersix <aoisupersix@gmail.com>
2019-05-17 22:25:30 +0900

    [add] プログラムで追加する構文定義の型定義を追加

--- gen/src/mapGrammarDefinition.ts
+++ gen/src/mapGrammarDefinition.ts
@@ -40,6 +40,49 @@ export interface MapStatement {
   skip_test: boolean | undefined
   // #endregion
 
+  // #region プログラムから追加する項目
+
+  /**
+   * 小文字のマップ要素名
+   */
+  elem_lower: string
+
+  /**
+   * 小文字のマップ副要素名
+   */
+  sub_elem_lower: string
+
+  /**
+   * 小文字の関数名
+   */
+  func_lower: string | null
+
+  /**
+   * 構文タイプ1か？
+   */
+  syntax1: boolean
+
+  /**
+   * 構文タイプ2か？
+   */
+  syntax2: boolean
+
+  /**
+   * 構文タイプ3か？
+   */
+  syntax3: boolean
+
+  /**
+   * 関数名を持たないステートメントか？
+   */
+  nofunc: boolean
+
+  /**
+   * 引数を持たないステートメントか？
+   */
+  noarg: boolean
+  // #endregion
+
 }
 
 /**
@@ -47,6 +90,7 @@ export interface MapStatement {
  */
 export interface Argument {
 
+  // #region 最初から設定してある項目
   /**
    * 引数名
    */
@@ -66,4 +110,29 @@ export interface Argument {
    * 省略可能な引数か？
    */
   opt: boolean
+  // #endregion
+
+  // #region プログラムから追加する項目
+
+  /**
+   * 最後の引数か？
+   * 最後の引数以外はカンマを付ける関係で使う
+   */
+  last: boolean | null
+
+  /**
+   * テスト用値(構文出力用)
+   */
+  test_value_map_grammar: string
+
+  /**
+   * テスト用値
+   */
+  test_value_map_grammar_non_quote: string
+
+  /**
+   * C#用テスト値
+   */
+  test_value_csharp: string
+  // #endregion
 }
