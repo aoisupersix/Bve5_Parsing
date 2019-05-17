343feea3c4995df062179b202cbe5044abd4aa1a
aoisupersix <aoisupersix@gmail.com>
2019-05-17 13:01:05 +0900

    [add] TSLintを導入

--- gen/src/create.ts
+++ gen/src/create.ts
@@ -2,10 +2,10 @@ import * as fs from 'fs'
 import * as yaml from 'js-yaml'
 
 function hello(name: string): string {
-    return `Hello, ${name}!`
+  return `Hello, ${name}!`
 }
 
-const file = fs.readFileSync(`${__dirname}/mapgrammar.yaml`, "utf8")
+const file = fs.readFileSync(`${__dirname}/mapgrammar.yaml`, 'utf8')
 const mapDef = yaml.safeLoad(file)
 
 console.log(mapDef)
