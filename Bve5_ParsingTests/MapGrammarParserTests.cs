5240fa8f13cedfbe38c6b7de20a13c61bb9c05cb
aoisupersix <aoisupersix@gmail.com>
2019-06-08 21:30:48 +0900

    [fix] MapGrammarParserのテストを通るように修正

--- Bve5_ParsingTests/MapGrammarParserTests.cs
+++ Bve5_ParsingTests/MapGrammarParserTests.cs
@@ -79,7 +79,8 @@ namespace Bve5_ParsingTests
             Assert.NotNull(data);
             Assert.Equal("2.02", data.Version);
             Assert.Single(data.Statements);
-            Assert.Equal("0", data.Statements[0].ToSyntaxData().Arguments["radius"] as string);
+            var val = data.Statements[0].ToSyntaxData().Arguments["radius"] as double?;
+            Assert.Equal(0, val.Value);
         }
     }
 }
