1806e5966a3d5265840e6fcd9d7127513faae9c7
aoisupersix <aoisupersix@gmail.com>
2019-07-24 12:56:33 +0900

    [add] convertMapStatementの型変換処理だけ作成

--- gen/src/converter/convertMapStatement.ts
+++ gen/src/converter/convertMapStatement.ts
@@ -1,6 +1,19 @@
 import { IMapDefinition } from '../definition/iMapDefinition'
 import { IMapStatement } from '../definition/iMapStatement'
 
-export const convertMapStatements = (mapDefinitions: IMapDefinition[]): void => {
-  console.log('nop')
+/**
+ * 引数に指定された全てのマップ構文定義をIMapStatementに変換します。
+ * @param mapDefinitions 変換対象のマップ構文定義
+ */
+export const convertMapStatements = (mapDefinitions: IMapDefinition[]): IMapStatement[] => {
+  return mapDefinitions.map(mapDef => convertMapStatemnet(mapDef))
+}
+
+/**
+ * マップ構文定義から必要な情報を付加したIMapStatementを返します。
+ * @param mapDefinition マップ構文定義(yaml)
+ */
+export const convertMapStatemnet = (mapDefinition: IMapDefinition): IMapStatement => {
+  const statement: IMapStatement = <any>mapDefinition
+  return statement
 }
