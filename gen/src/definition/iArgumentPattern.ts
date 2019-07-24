45ff7634d4389fc5d9eb039546479805b93729fe
aoisupersix <aoisupersix@gmail.com>
2019-07-24 13:18:21 +0900

    [add] 引数のパターンを意味するIArgumentPatternを追加

---
+++ gen/src/definition/iArgumentPattern.ts
@@ -0,0 +1,27 @@
+import { IArgument } from './iArgument'
+
+/**
+ * 構文の引数がとり得るパターン定義
+ * テストの生成時に使います。
+ */
+export interface IArgumentPattern {
+  /**
+   * 対象の引数
+   */
+  args: IArgument
+
+  /**
+   * テスト対象のBve5マップバージョン
+   */
+  version: string
+
+  /**
+   * テストにV1Parserを使用するか？
+   */
+  useV1Parser: boolean
+
+  /**
+   * テストにV2Parserを使用するか?
+   */
+  useV2Parser: boolean
+}
