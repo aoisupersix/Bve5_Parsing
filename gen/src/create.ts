0122c91d7c868a6a3c71e6efbea67e6ce3c7ebe7
aoisupersix <aoisupersix@gmail.com>
2019-07-26 07:53:15 +0900

    [add] 書き出し処理を追加

--- gen/src/create.ts
+++ gen/src/create.ts
@@ -1,7 +1,10 @@
 import * as fs from 'fs'
 import * as yaml from 'js-yaml'
+import * as mustache from 'mustache'
+
 import { IMapDefinition } from './definition/iMapDefinition';
 import { convertMapStatements } from './converter/convertMapStatement';
+import { IMapStatement } from './definition/iMapStatement';
 
 const file = fs.readFileSync(`${__dirname}/mapgrammar.yaml`, 'utf8')
 const mapDefs: IMapDefinition[] = yaml.safeLoad(file)
@@ -11,3 +14,8 @@ const json = JSON.stringify(convertedMapDefs)
 console.log(json)
 
 console.log(convertedMapDefs)
+
+const parse = (template: string, outputPath: string, statements: IMapStatement[]): void {
+  const output = mustache.render(template, statements)
+  fs.writeFile(outputPath, output, { encoding: 'utf-8' }, _ => console.log(`${outputPath} generation completed.`))
+}
