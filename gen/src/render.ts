221dd268745e821e6812abc6c1ff648aa8111755
aoisupersix <aoisupersix@gmail.com>
2019-07-27 09:44:59 +0900

    [change] renderでもデータにはIMapDataを使うようにした

--- gen/src/render.ts
+++ gen/src/render.ts
@@ -2,13 +2,13 @@ import * as fs from 'fs'
 import * as mustache from 'mustache'
 import stripBom from 'strip-bom'
 import { promisify } from 'util'
-import { IMapStatement } from './definition/iMapStatement';
+import { IMapData } from './definition/iMapData';
 
 /**
  * ステートメントをテンプレートに書き出します。
  * @param template Mustacheのレンダー先
- * @param statements 出力対象のステートメント
+ * @param mapData 出力対象のデータ
  */
-export const render = (template: string, statements: IMapStatement[]): string => {
-  return mustache.render(stripBom(template), statements)
+export const render = (template: string, mapData: IMapData): string => {
+  return mustache.render(stripBom(template), mapData)
 }
