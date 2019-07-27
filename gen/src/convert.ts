d1b4984ddc2f42c502c5dfa471b03aa295b2d300
aoisupersix <aoisupersix@gmail.com>
2019-07-27 09:43:34 +0900

    [change] convert()ではIMapDataを生成して返すようにした

--- gen/src/convert.ts
+++ gen/src/convert.ts
@@ -1,11 +1,13 @@
 import { IMapDefinition } from './definition/iMapDefinition'
-import { IMapStatement } from './definition/iMapStatement';
 import { convertMapStatemnet } from './converter/convertMapStatement';
+import { IMapData } from './definition/iMapData';
+import { createMapData } from './converter/createMapData';
 
 /**
- * 引数に指定された全てのマップ構文定義をIMapStatementに変換します。
+ * 引数に指定された全てのマップ構文定義をIMapDataに変換します。
  * @param mapDefinitions 変換対象のマップ構文定義
  */
-export const convert = (mapDefinition: IMapDefinition[]): IMapStatement[] => {
-  return mapDefinition.map(mapDef => convertMapStatemnet(mapDef))
+export const convert = (mapDefinition: IMapDefinition[]): IMapData => {
+  const statements =  mapDefinition.map(mapDef => convertMapStatemnet(mapDef))
+  return createMapData(statements)
 }
